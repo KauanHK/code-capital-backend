caf95877922433893c764186d809e2988ace5a73
Khaee Winterfeld <wkhaee@gmail.com>
2026-03-22 11:09:06 -0300

    feat: add middleware

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.config import settings
 from app.routers import categories, clients, services, transactions
 
 app = FastAPI(title=settings.API_TITLE, version=settings.API_VERSION)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://localhost:3001"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/health")
 def healthcheck() -> dict[str, str]:
